d99b3a1ad90c7dba738fb759a7a1466b5724db75
Saurabh Chandra Rai <35683286+SAURABHRAI110@users.noreply.github.com>
2025-12-10 16:27:44 +0530

    first commit
    first commit

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { PackagesModule } from '../../libs/packages/packages.module';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [PackagesModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  public socialLinks: any = {
+    'instagram': 'https://www.instagram.com/officialdeecreations/',
+    'youtube': 'https://www.youtube.com/@deecreationsofficial',
+    'linkedin': 'https://www.linkedin.com/in/deecreationsofficial/',
+    'facebook': 'https://www.facebook.com/officialdeecreations'
+  }
 
+  constructor() {
+
+  }
+
+  goToLink(type: any) {
+    if (type) {
+      window.open(this.socialLinks[type], '_blank');
+    }
+  }
+
+  goToWhatsapp() {
+    // Phone number you want to send the message to
+    var phoneNumber = "7304564563";
+    // Message you want to send
+    var message = "Hello there..";
+    // Encode the phone number and message for URL
+    var encodedPhoneNumber = encodeURIComponent(phoneNumber);
+    var encodedMessage = encodeURIComponent(message);
+    // Construct the URL for sending the message via WhatsApp
+    var whatsappUrl = "https://wa.me/" + encodedPhoneNumber + "?text=" + encodedMessage;
+    // Redirect the user to WhatsApp
+    window.location.href = whatsappUrl;
+  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { PackagesModule } from '../../libs/packages/packages.module';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [PackagesModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  public menuList: any = [{
+    menu_name: 'Contact Us',
+    menu_path: '/contact'
+  }, {
+    menu_name: 'About',
+    menu_path: '/about'
+  },
+  {
+    menu_name: 'Work',
+    menu_path: '/myworks'
+  },
+  {
+    menu_name: 'Services',
+    menu_path: '/services'
+  },
+  {
+    menu_name: 'Home',
+    menu_path: '/'
+  }
+  ]
 
+  constructor(private router: Router) { }
+
+  isActive(menuPath: any) {
+    return this.router.url === menuPath;
+  }
+
+  removeActiveClass(): void {
+    document.querySelectorAll('.menu li').forEach((element) => {
+      element.classList.remove('active');
+    });
+  }
 }
