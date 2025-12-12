a75b9421d0bfb3632aa4d5e5306c456e4820c01e
Aman Sharma <Aman733t>
2025-12-12 16:42:21 +0530

    changes for scroll

--- src/app/modules/contact-us/contact-us.component.ts
+++ src/app/modules/contact-us/contact-us.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { FooterComponent } from "../../components/footer/footer.component";
 import { PackagesModule } from '../../libs/packages/packages.module';
+import { ScrolldownComponent } from "../../components/scrolldown/scrolldown.component";
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, PackagesModule],
+  imports: [HeaderComponent, FooterComponent, PackagesModule, ScrolldownComponent],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss'
 })
--- src/app/modules/project-details/project-details.component.ts
+++ src/app/modules/project-details/project-details.component.ts
@@ -2,14 +2,25 @@ import { Component } from '@angular/core';
 import { PackagesModule } from '../../libs/packages/packages.module';
 import { HeaderComponent } from "../../components/header/header.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { ScrolldownComponent } from '../../components/scrolldown/scrolldown.component';
 
 @Component({
   selector: 'app-project-details',
   standalone: true,
-  imports: [PackagesModule, HeaderComponent, FooterComponent],
+  imports: [PackagesModule, HeaderComponent, FooterComponent, ScrolldownComponent],
   templateUrl: './project-details.component.html',
   styleUrl: './project-details.component.scss'
 })
 export class ProjectDetailsComponent {
 
+  constructor(){
+
+  }
+
+  scroller() {
+    let offset = 0;
+    let element: any = document.getElementById("h-section3");
+    let scroolValue = element.getBoundingClientRect().top + window.pageYOffset + offset;
+    window.scrollTo({ top: scroolValue, behavior: 'smooth' })
+  }
 }
