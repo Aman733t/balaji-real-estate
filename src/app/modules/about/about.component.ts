1d95986514eea908445f9963ee3f3c494b8517f1
Saurabh Chandra Rai <35683286+SAURABHRAI110@users.noreply.github.com>
2025-12-12 15:29:13 +0530

    +scroll down

--- src/app/modules/about/about.component.ts
+++ src/app/modules/about/about.component.ts
@@ -2,14 +2,25 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { FooterComponent } from "../../components/footer/footer.component";
 import { PackagesModule } from '../../libs/packages/packages.module';
+import { ScrolldownComponent } from "../../components/scrolldown/scrolldown.component";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent,PackagesModule],
+  imports: [HeaderComponent, FooterComponent, PackagesModule, ScrolldownComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  constructor() {
+
+  }
+
+  scroller() {
+    let offset = 0;
+    let element: any = document.getElementById("h-section3");
+    let scroolValue = element.getBoundingClientRect().top + window.pageYOffset + offset;
+    window.scrollTo({ top: scroolValue, behavior: 'smooth' })
+  }
 
 }
--- src/app/modules/contact-us/contact-us.component.ts
+++ src/app/modules/contact-us/contact-us.component.ts
@@ -6,10 +6,19 @@ import { PackagesModule } from '../../libs/packages/packages.module';
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent,PackagesModule],
+  imports: [HeaderComponent, FooterComponent, PackagesModule],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss'
 })
 export class ContactUsComponent {
+  constructor() {
 
+  }
+
+  scroller() {
+    let offset = 0;
+    let element: any = document.getElementById("h-section3");
+    let scroolValue = element.getBoundingClientRect().top + window.pageYOffset + offset;
+    window.scrollTo({ top: scroolValue, behavior: 'smooth' })
+  }
 }
